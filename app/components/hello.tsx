459b0c0bfbac1fd888ff1ad34f91b91716eca137
vinay_hv <vinayhv66@gmail.com>
2025-05-15 11:49:45 +0530

    dashboard added

---
+++ app/components/hello.tsx
@@ -0,0 +1,12 @@
+function Hello(){
+  console.log("im a client component");
+  return(
+<div>
+    <h1>
+      hello
+    </h1>
+    </div>
+
+  ); 
+}
+export default Hello
---
+++ app/dashboard/analytics/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      analytics
+    </div>
+  )
+}
+
+export default page
